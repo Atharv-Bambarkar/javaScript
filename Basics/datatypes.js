fae95682620e8b2913e008bf45e1892ac3a2f9a9
Atharv-Bambarkar <109845006+Atharv-Bambarkar@users.noreply.github.com>
2024-01-09 04:00:09 +0000

    datatypes and variables learned

---
+++ Basics/datatypes.js
@@ -0,0 +1,29 @@
+
+"use strict"
+//number 
+let number =10;
+
+//bigint
+const big=BigInt(244164667646649549)
+
+//string
+let name = `atharv`
+
+
+//null
+let hi= null;
+
+
+//boolean
+let isLoggedIn= false
+
+let undefined
+
+
+
+console.table([typeof(number),typeof(big),typeof(name),typeof(hi),typeof(isLoggedIn),typeof(undefined)])
+
+
+
+
+
---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+var userName =`Atharv`
+const accountId = 12345
+let accountEmail=`Atharv12345@gmail.com`
+let accountState;
+
+/*
+always use let instead of var
+
+*/
+
+console.table([userName,accountId,accountEmail,accountState])
