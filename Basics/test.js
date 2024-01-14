58145ccdbccd987cbfb7e95766130d9b6e4ac3c4
Atharv-Bambarkar <109845006+Atharv-Bambarkar@users.noreply.github.com>
2024-01-14 14:22:42 +0000

    new string methods

--- Basics/test.js
+++ Basics/test.js
@@ -1,7 +1,35 @@
 const name = `Atharv`
 const lastName =`Bambarkar`
-console.log("🚀 ~ name:", name)
+console.log("🚀 ~ name:", name[0])
 
 console.log("🚀 ~ lastName:", lastName)
+
+
+const newString = `   Atharrvvv`
+console.log("🚀 ~ newString:", newString.trim())
+console.log("🚀 ~ newString:", newString.trimStart())
+
+
+
+let url=`https://atharv.com/atharv%20bambarkar`
+
+console.log("🚀 ~ url:", url.includes('atharv'))
+console.log("🚀 ~ url:", url.replace('%20','_'))
+
+const a =`Atharv  `
+
+console.log("🚀 ~ a:", a.__proto__)
+console.log("🚀 ~ a:", a.blink())
+console.log("🚀 ~ a:", a.split(` `,3))   //split(seperater,limit)
+const ab = a.trim()
+
+
+if(ab  === name){
+    console.log(`Matched`)
+}
+else{
+
+    console.log(`not matched`)
+}
 
 
