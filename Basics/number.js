3fcfeff929122909041600a755b0d55db07f0401
Atharv-Bambarkar <109845006+Atharv-Bambarkar@users.noreply.github.com>
2024-01-14 15:03:38 +0000

    learned maths

---
+++ Basics/number.js
@@ -0,0 +1,23 @@
+const score = 506.55464;
+console.log("🚀 ~ score:", score.toString())
+console.log("🚀 ~ score:", score.toPrecision(3))
+console.log("🚀 ~ score:", score.toFixed(2))
+
+let hundreds=10_00_000;
+console.log("🚀 ~ score:", hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+
+console.log (Math.abs(-4))
+console.log (Math.round(4.3))
+console.log (Math.ceil(4.2))
+console.log (Math.floor(4.2))
+console.log(Math.pow(2,3));
+console.log(Math.random()*100)
+
+
+TODO:
+const min=1;
+const max =6;
+console.log(Math.round((Math.random()*(max-min+1)+min)))  
