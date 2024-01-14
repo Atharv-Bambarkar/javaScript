f46e813a52701c7c23ac32162bd0cdaf2295f7a0
Atharv-Bambarkar <109845006+Atharv-Bambarkar@users.noreply.github.com>
2024-01-14 15:21:31 +0000

    learned dates

---
+++ Basics/dates.js
@@ -0,0 +1,8 @@
+ const myDate = new Date();
+ console.log(myDate.toTimeString());
+ console.log(myDate.toJSON());
+ console.log(myDate.toDateString());
+ console.log(myDate.toString());
+
+ const myCreatedDate = new Date(2024,0,28,5,56,55) //Sun Jan 28 2024 05:56:55 GMT+0000 (Coordinated Universal Time)
+ console.log(myCreatedDate.toString());
